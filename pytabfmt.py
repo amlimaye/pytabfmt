c5360102eb01a9a49480027e0e7b599c2304832b
Aditya Limaye <aditya.limaye@gmail.com>
2018-11-21 16:53:08 -0500

    initial commit

---
+++ pytabfmt.py
@@ -0,0 +1,86 @@
+import sys
+import io
+import IPython
+
+class LatexTable():
+    def __init__(self, columns, fmt_strings):
+        self.rows = []
+        self.column_titles = columns
+        self.fmt_strings = fmt_strings
+
+
+    def add_row(self, row):
+        assert len(row) == len(self.column_titles)
+        self.rows.append(row)
+
+
+    def write_buffer(self, f):
+        colspec = format_column_specification(self.column_titles)
+        f.write('\\begin{tabular}{%s}\n' % colspec)
+
+        header = format_header_string(self.column_titles)
+        f.write(header)
+
+        for idx, r in enumerate(self.rows):
+            last_row = determine_last_row(idx, self.rows)
+            fmtd = format_row(self.fmt_strings, r, last_row)
+            f.write(fmtd)
+
+        f.write('\\end{tabular}\n')
+
+
+    def write(self, fname):
+        with open(fname, 'w') as f:
+            self.write_buffer(f)
+
+
+    def print_out(self):
+        self.write_buffer(sys.stdout)
+
+
+    def __repr__(self):
+        pipe = io.StringIO()
+        self.write_buffer(pipe)
+        contents = pipe.getvalue()
+        pipe.close()
+        return contents
+
+
+def determine_last_row(idx, rows):
+    return idx == (len(rows) - 1)
+
+
+def format_column_specification(col_titles):
+    column_specs = ['l' for _ in col_titles]
+    colspec = '|'.join(column_specs)
+    return colspec
+
+
+def format_header_string(headers):
+    header_strings = ['%s & ' % h for h in headers]
+    headers_concat = ''.join(header_strings)
+    final = headers_concat[:-2] + '\\\\ \\hline \n'
+
+    return final
+
+
+def format_row(fmts, entries, last_row=False):
+    format_strings = []
+    for idx, fs in enumerate(fmts):
+        if idx == 0:
+            format_strings.append('%s' % fs)
+        else:
+            format_strings.append('$%s$' % fs)
+
+    formatted_entries = []
+    for fs, entry in zip(format_strings, entries):
+        formatted_entries.append(fs % entry + ' & ')
+
+    joined = ''.join(formatted_entries)
+
+    if not last_row:
+        final = joined[:-2] + ' \\\\ \n'
+    else:
+        final = joined[:-2] + ' \n'
+
+    return final
---
+++ tests/test.py
@@ -0,0 +1,30 @@
+import pytest
+import glob
+import os
+import filecmp
+import io
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+import pytabfmt as ptf
+
+fixtures_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+@pytest.fixture()
+def example_table():
+    path = os.path.join(fixtures_dir, 'example_table_1.txt')
+    return path
+
+
+def test_simple_filewrite(example_table, tmpdir):
+    example = example_table
+    tbl = ptf.LatexTable('Teacher Kyle Adi Vincent'.split(),
+                         ['%s', '%0.2f', '%0.2f', '%0.2f'])
+    tbl.add_row(['DB', 0.0, 0.0, 5.0])
+    tbl.add_row(['Swan', 5.0, 5.0, 0.0])
+    tbl.add_row(['Bazant', 5.0, 5.0, 0.0])
+
+    fpath = tmpdir.join('test.txt')
+    tbl.write(fpath)
+
+    assert filecmp.cmp(fpath, example)
